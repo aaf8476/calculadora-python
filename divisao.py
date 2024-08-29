1c26674dc08aab0663cb9ee009f8d34fc6751cfa
Gabi da Silva <gabriela.silva.07.11@gmail.com>
2024-08-29 09:53:10 -0300

    divisao-multiplicacao

---
+++ divisao.py
@@ -0,0 +1,6 @@
+def dividir (num1, num2):
+    if num2 != 0:
+      return num1/num2
+    else:
+      return "Erro, digite um número  diferente de Zero." 
+
---
+++ multiplicacao.py
@@ -0,0 +1,3 @@
+def multiplicar (num1, num2):
+    return num1 * num2
+
